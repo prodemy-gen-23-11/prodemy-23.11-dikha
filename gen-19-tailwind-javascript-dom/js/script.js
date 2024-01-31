b6d5a8942d345778aa137033116f5f7d852bd4d3
Dikha Khameswara <dikhakhameswara10@gmail.com>
2024-01-31 16:09:17 +0700

    updates

--- gen-19-tailwind-javascript-dom/js/script.js
+++ gen-19-tailwind-javascript-dom/js/script.js
@@ -1,7 +1,9 @@
 
+//Declare Elements for section Produk
 let produkImg = document.getElementById('produk')
 let mainProdukImg = produkImg.getElementsByTagName('div')[0].getElementsByTagName('img')[0]
 let childProdukImg = produkImg.getElementsByTagName('div')[1].getElementsByTagName('img')
+console.log(produkImg)
 
 function klik(num){
     mainProdukImg.setAttribute('src',childProdukImg[num].src)
